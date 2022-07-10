6c94e6b73f8fbf2429a038a1a18fb4b811d8e7db
chungnguyen <chungnguyen10012000@gmail.com>
2022-07-11 00:10:36 +0700

    add entity teacher register teach course

---
+++ src/main/java/com/app/kidsdrawing/entity/UserRegisterTeachCourse.java
@@ -0,0 +1,62 @@
+package com.app.kidsdrawing.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "User_Register_Teach_Course")
+public class UserRegisterTeachCourse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    @JoinColumn(name = "teacher_id", referencedColumnName = "id")
+    private User teacher;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    @JoinColumn(name = "course_id", referencedColumnName = "id")
+    private Course course;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    @JoinColumn(name = "reviewer_id", referencedColumnName = "id")
+    private User reviewer;
+
+    @Column(name = "status")
+    private Boolean status;
+
+    @Builder.Default()
+    @Column(name = "create_time")
+    @CreationTimestamp
+    private LocalDateTime createTime = LocalDateTime.now();
+
+    @Builder.Default()
+    @Column(name = "update_time")
+    @UpdateTimestamp
+    private LocalDateTime updateTime = LocalDateTime.now();
+}
