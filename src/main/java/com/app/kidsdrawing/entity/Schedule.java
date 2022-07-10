c6aea8c99c99850278677b8660c366998c2b1a3b
chungnguyen <chungnguyen10012000@gmail.com>
2022-07-11 00:09:05 +0700

    add entity schedule

---
+++ src/main/java/com/app/kidsdrawing/entity/Schedule.java
@@ -0,0 +1,59 @@
+package com.app.kidsdrawing.entity;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Schedule")
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Builder.Default()
+    @Column(name = "create_time")
+    @CreationTimestamp
+    private LocalDateTime create_time = LocalDateTime.now();
+
+    @Builder.Default()
+    @Column(name = "update_time")
+    @UpdateTimestamp
+    private LocalDateTime update_time = LocalDateTime.now();
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    @JoinColumn(name = "creator_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToMany(mappedBy="schedule")
+    private Set<ScheduleItem> scheduleItems;
+}
