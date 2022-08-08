3d3bd2200199a93894f036bf30df03e73337dd33
chungnguyen <chungnguyen10012000@gmail.com>
2022-08-08 23:37:25 +0700

    logic for add section for class, remove schedule view

--- src/main/java/com/app/kidsdrawing/service/impl/SemesterServiceImpl.java
+++ src/main/java/com/app/kidsdrawing/service/impl/SemesterServiceImpl.java
@@ -1,9 +1,6 @@
 package com.app.kidsdrawing.service.impl;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -18,7 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.app.kidsdrawing.dto.CreateHolidayResquest;
+import com.app.kidsdrawing.dto.CreateHolidayRequest;
 import com.app.kidsdrawing.dto.CreateSemesterRequest;
 import com.app.kidsdrawing.dto.GetSemesterResponse;
 import com.app.kidsdrawing.entity.Semester;
@@ -26,22 +23,17 @@ import com.app.kidsdrawing.entity.SemesterCourse;
 import com.app.kidsdrawing.entity.User;
 import com.app.kidsdrawing.entity.Class;
 import com.app.kidsdrawing.entity.Course;
-import com.app.kidsdrawing.entity.LessonTime;
+import com.app.kidsdrawing.entity.Holiday;
 import com.app.kidsdrawing.entity.Schedule;
-import com.app.kidsdrawing.entity.ScheduleItem;
 import com.app.kidsdrawing.entity.Section;
 import com.app.kidsdrawing.entity.SectionTemplate;
 import com.app.kidsdrawing.entity.UserRegisterJoinSemester;
 import com.app.kidsdrawing.entity.UserRegisterTeachSemester;
 import com.app.kidsdrawing.exception.EntityNotFoundException;
 import com.app.kidsdrawing.repository.ClassRepository;
-//import com.app.kidsdrawing.repository.CourseRepository;
-//import com.app.kidsdrawing.repository.LessonTimeRepository;
-import com.app.kidsdrawing.repository.ScheduleItemRepository;
+import com.app.kidsdrawing.repository.HolidayRepository;
 import com.app.kidsdrawing.repository.SectionRepository;
 import com.app.kidsdrawing.repository.SectionTemplateRepository;
-//import com.app.kidsdrawing.repository.ScheduleRepository;
-import com.app.kidsdrawing.repository.SemesterCourseRepository;
 import com.app.kidsdrawing.repository.SemesterRepository;
 import com.app.kidsdrawing.repository.TeacherTeachSemesterRepository;
 import com.app.kidsdrawing.repository.UserRegisterJoinSemesterRepository;
@@ -56,21 +48,15 @@ import lombok.RequiredArgsConstructor;
 public class SemesterServiceImpl implements SemesterService {
 
     private final SemesterRepository semesterRepository;
-    private final SemesterCourseRepository semesterCourseRepository;
     private final UserRegisterJoinSemesterRepository userRegisterJoinSemesterRepository;
     private final TeacherTeachSemesterRepository teacherTeachSemesterRepository;
     private final ClassRepository classRepository;
     private final UserRepository userRepository;
-    //private final ScheduleRepository scheduleRepository;
-    private final ScheduleItemRepository scheduleItemRepository;
     private final SectionTemplateRepository sectionTemplateRepository;
     private final SectionRepository sectionRepository;
-    //private final LessonTimeRepository lessonTimeRepository;
-    //private final CourseRepository courseRepository;
-    private static int counter = 0;
-    private static int total_count = 0;
-    private static int total_section_count = 0;
+    private final HolidayRepository holidayRepository;
 
+    private static int counter = 0;
     @Override
     public ResponseEntity<Map<String, Object>> getAllSemester() {
         List<GetSemesterResponse> allSemesterResponses = new ArrayList<>();
@@ -123,7 +109,7 @@ public class SemesterServiceImpl implements SemesterService {
     }
 
     @Override
-    public ResponseEntity<Map<String, Object>> setCalenderForSemester(Long id, CreateHolidayResquest createHolidayResquest) {
+    public Long setCalenderForSemester(Long id, CreateHolidayRequest createHolidayResquest) {
         // Lấy học kì
         Optional<Semester> semesterOpt = semesterRepository.findById(id);
         Semester semester = semesterOpt.orElseThrow(() -> {
@@ -133,18 +119,14 @@ public class SemesterServiceImpl implements SemesterService {
         List<Course> allCourseResponses = new ArrayList<>();
         //List<Course> pageCourse = courseRepository.findAll();
         List<SemesterCourse> allSemesterCourseResponses = new ArrayList<>();
-        List<SemesterCourse> pageSemesterCourse = semesterCourseRepository.findAll();
         List<Schedule> allScheduleResponses = new ArrayList<>();
         //List<Schedule> pageSchedule = scheduleRepository.findAll();
-        List<ScheduleItem> pageScheduleItem = scheduleItemRepository.findAll();
         //List<LessonTime> pageLessonTime = lessonTimeRepository.findAll();
 
-        pageSemesterCourse.forEach(semester_course -> {
-            if (semester_course.getSemester().getId() == id){
-                allSemesterCourseResponses.add(semester_course);
-                allScheduleResponses.add(semester_course.getSchedule());
-                allCourseResponses.add(semester_course.getCourse());
-            }
+        semester.getSemesterCourse().forEach(semester_course -> {
+            allSemesterCourseResponses.add(semester_course);
+            allScheduleResponses.add(semester_course.getSchedule());
+            allCourseResponses.add(semester_course.getCourse());
         });
 
         List<Class> listClass = classRepository.findAll();
@@ -159,121 +141,21 @@ public class SemesterServiceImpl implements SemesterService {
             allClassOfSemesterCourseResponses.add(list_class);
         });
 
-        List<List<ScheduleItem>> allScheduleItemResponses = new ArrayList<>();
-        List<Integer> totalDayOfWeek = new ArrayList<>();
-        allScheduleResponses.forEach(schedule -> {
-            total_count = 0;
-            List<ScheduleItem> scheduleItemResponses = new ArrayList<>();
-            pageScheduleItem.forEach(schedule_item -> {
-                if (schedule_item.getSchedule().getId() == schedule.getId()){
-                    scheduleItemResponses.add(schedule_item);
-                    total_count ++;
-                }
-            });
-            allScheduleItemResponses.add(scheduleItemResponses);
-            totalDayOfWeek.add(total_count);
+        createHolidayResquest.getTime().forEach(holiday -> {
+            Holiday saveHoliday = Holiday.builder()
+                .day(holiday)
+                .semester(semester)
+                .build();
+            holidayRepository.save(saveHoliday);
         });
-
-        List<List<LessonTime>> allLessonTimeResponses = new ArrayList<>();
-        allScheduleItemResponses.forEach(schedule_items -> {
-            List<LessonTime> lessonTimeResponses = new ArrayList<>();
-            schedule_items.forEach(schedule_item -> {
-                lessonTimeResponses.add(schedule_item.getLessonTime());
-            });
-            allLessonTimeResponses.add(lessonTimeResponses);
-        }); 
-
-        List<List<Map <String, List<LocalDateTime>>>> allCalendarForSemesterCourse = new ArrayList<>();
-        for (int index = 0; index < allScheduleItemResponses.size(); index++) {
-            Integer total_section = allCourseResponses.get(index).getNum_of_section();
-            List<Map <String, List<LocalDateTime>>> calendarForSemesterCourse = new ArrayList<>();
-            Integer total_week = (int) Math.ceil( total_section / totalDayOfWeek.get(index));
-            System.out.printf("total_week: %d\n", total_week);
-            total_section_count = 0;
-            for (int idx = 0; idx < allScheduleItemResponses.get(index).size(); idx++) {
-                counter = 0;
-                LocalDateTime start_time = semester.getStart_time();
-                Integer dayOfWeek = allScheduleItemResponses.get(index).get(idx).getDate_of_week();
-                LocalTime start_lessontime = allLessonTimeResponses.get(index).get(idx).getStart_time();
-                LocalTime end_lessontime = allLessonTimeResponses.get(index).get(idx).getEnd_time();
-                //LocalDateTime end_time = semester.getStart_time().plusWeeks(total_week);
-                if (dayOfWeek == 2){
-                    while (start_time.getDayOfWeek() != DayOfWeek.MONDAY){
-                        System.out.print(start_time.getDayOfWeek());
-                        start_time = start_time.plusDays(1);
-                    }
-                }
-                else if (dayOfWeek == 3){
-                    while (start_time.getDayOfWeek() != DayOfWeek.TUESDAY){
-                        System.out.print(start_time.getDayOfWeek());
-                        start_time = start_time.plusDays(1);
-                    }
-                }
-
-                else if (dayOfWeek == 4){
-                    while (start_time.getDayOfWeek() != DayOfWeek.WEDNESDAY){
-                        System.out.print(start_time.getDayOfWeek());
-                        start_time = start_time.plusDays(1);
-                    }
-                }
-
-                else if (dayOfWeek == 5){
-                    while (start_time.getDayOfWeek() != DayOfWeek.THURSDAY){
-                        System.out.print(start_time.getDayOfWeek());
-                        start_time = start_time.plusDays(1);
-                    }
-                }
-
-                else if (dayOfWeek == 6){
-                    while (start_time.getDayOfWeek() != DayOfWeek.FRIDAY){
-                        System.out.print(start_time.getDayOfWeek());
-                        start_time = start_time.plusDays(1);
-                    }
-                }
-
-                else if (dayOfWeek == 7){
-                    while (start_time.getDayOfWeek() != DayOfWeek.SATURDAY){
-                        System.out.print(start_time.getDayOfWeek());
-                        start_time = start_time.plusDays(1);
-                    }
-                }
-
-                else {
-                    while (start_time.getDayOfWeek() != DayOfWeek.SUNDAY){
-                        System.out.print(start_time.getDayOfWeek());
-                        start_time = start_time.plusDays(1);
-                    }
-                }
-                while (counter <= total_week && total_section_count < total_section) {
-                    /* if (startDate.equals(holiday) == false){
-                        System.out.println(startDate);
-                    } */
-                    Map<String, List<LocalDateTime>> hm = new HashMap<String, List<LocalDateTime>>();
-                    LocalDate  start_date = start_time.toLocalDate();
-                    if (createHolidayResquest.getTime().contains(start_date) == false){
-                        List <LocalDateTime> ele_day = new ArrayList<>();
-                        ele_day.add(start_lessontime.atDate(start_date));
-                        ele_day.add(end_lessontime.atDate(start_date));
-                        hm.put("Time", ele_day);
-                        calendarForSemesterCourse.add(hm);
-                        counter++;
-                        total_section_count ++;
-                    }
-                    start_time = start_time.plusWeeks(1);
-                }
-                
-            }
-            allCalendarForSemesterCourse.add(calendarForSemesterCourse);
-        }
 
         allClassOfSemesterCourseResponses.forEach( list_class -> {
             List<SectionTemplate> listSectionTemplate = sectionTemplateRepository.findAll();
             List<SectionTemplate> allSectionTemplate = new ArrayList<>();
-            total_section_count = 0;
             listSectionTemplate.forEach(section_template -> {
                 if (section_template.getCourse().getId() == list_class.get(0).getTeachSemester().getSemesterCourse().getCourse().getId()){
                     allSectionTemplate.add(section_template);
-                    total_section_count = section_template.getCourse().getNum_of_section();
+                    section_template.getCourse().getNum_of_section();
                 }
             });
             list_class.forEach(ele -> {
@@ -289,11 +171,7 @@ public class SemesterServiceImpl implements SemesterService {
                 });
             });
         }); 
-
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("calendar_semester_course", allCalendarForSemesterCourse);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return id;
     }
 
     public static int[][] foo(int N, int x, int m, int M){ //ham can viet
@@ -364,12 +242,15 @@ public class SemesterServiceImpl implements SemesterService {
 
     @Override
     public Long setClassForSemester(Long id, int partion, int min, int max) {
+        // Lấy học kì
+        Optional<Semester> semesterOpt = semesterRepository.findById(id);
+        Semester semester = semesterOpt.orElseThrow(() -> {
+            throw new EntityNotFoundException("exception.Semester.not_found");
+        });
+
         List<SemesterCourse> allSemesterCourseResponses = new ArrayList<>();
-        List<SemesterCourse> pageSemesterCourse = semesterCourseRepository.findAll();
-        pageSemesterCourse.forEach(semester_course -> {
-            if (semester_course.getSemester().getId() == id){
-                allSemesterCourseResponses.add(semester_course);
-            }
+        semester.getSemesterCourse().forEach(semester_course -> {
+            allSemesterCourseResponses.add(semester_course);
         });
 
         List<Class> listClass = classRepository.findAll();
@@ -437,14 +318,16 @@ public class SemesterServiceImpl implements SemesterService {
                     int idx = list_group[i][j] - 1;
                     validUserRegisterSemesters.add(allUserRegisterJoinSemesters.get(idx));
                 }
-                Class savedClass = Class.builder()
-                    .user(user)
-                    .teachSemester(allUserRegisterTeachSemesters.get(i))
-                    .security_code(String.valueOf(i))
-                    .name(String.valueOf(i))
-                    .userRegisterJoinSemesters(new HashSet<>(validUserRegisterSemesters))
-                    .build();
-                classRepository.save(savedClass);
+                if (i < allUserRegisterTeachSemesters.size()){
+                    Class savedClass = Class.builder()
+                        .user(user)
+                        .teachSemester(allUserRegisterTeachSemesters.get(i))
+                        .security_code(String.valueOf(i))
+                        .name(String.valueOf(i))
+                        .userRegisterJoinSemesters(new HashSet<>(validUserRegisterSemesters))
+                        .build();
+                    classRepository.save(savedClass);
+                }
             }
         });
         return id;
