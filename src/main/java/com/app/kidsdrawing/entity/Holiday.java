38894af924c10f9ff43669a43d559bae1ca08f5f
chungnguyen <chungnguyen10012000@gmail.com>
2022-08-08 23:33:40 +0700

    add holiday entity

---
+++ src/main/java/com/app/kidsdrawing/entity/Holiday.java
@@ -0,0 +1,41 @@
+package com.app.kidsdrawing.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "holiday")
+public class Holiday {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "day")
+    private LocalDate day;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.DETACH)
+    @JoinColumn(name = "semester_id", referencedColumnName = "id")
+    private Semester semester;
+}
