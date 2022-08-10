41f55a65977bfa40e032cb9feba8f3c955fa7e86
chungnguyen <chungnguyen10012000@gmail.com>
2022-08-10 22:05:22 +0700

    custom get user role

--- src/main/java/com/app/kidsdrawing/config/SecurityConfig.java
+++ src/main/java/com/app/kidsdrawing/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeHttpRequests()
                 .antMatchers("/api/v1/auth/**").permitAll()
-                .antMatchers("/api/v1/user/**").hasAnyAuthority("ADMIN_USER")
+                .antMatchers("/api/v1/user/**").permitAll()
                 //.antMatchers("/api/v1/user/admin/**").hasAnyAuthority("SUPER_ADMIN_USER")
                 //.antMatchers("/api/v1/user/staff/**").hasAnyAuthority("SUPER_ADMIN_USER","ADMIN_USER")
                 //.antMatchers("/api/v1/user/teacher/**").hasAnyAuthority("SUPER_ADMIN_USER","ADMIN_USER","STAFF_USER")
