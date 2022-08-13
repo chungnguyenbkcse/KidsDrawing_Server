3630f9c930bbe900219a17f0b405c450e66dac33
chungnguyen <chungnguyen10012000@gmail.com>
2022-08-13 20:06:57 +0700

    add reset time

--- src/main/java/com/app/kidsdrawing/util/AuthUtil.java
+++ src/main/java/com/app/kidsdrawing/util/AuthUtil.java
@@ -39,8 +39,8 @@ public class AuthUtil {
 
     @Value("${jwt_secret}")
     private String secret;
-    private long accessTokenDuration = 100;
-    private long refreshTokenDuration = 100;
+    private long accessTokenDuration = 600000;
+    private long refreshTokenDuration = 3600000;
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
 
