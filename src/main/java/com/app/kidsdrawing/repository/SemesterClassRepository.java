50c7bd5398732a53e860ff895a05e85d5814ea8a
chungnguyent2601 <chungnguyen10012000@gmail.com>
2022-11-11 14:50:39 +0700

    fix performance for get semester course for admin

--- src/main/java/com/app/kidsdrawing/repository/SemesterClassRepository.java
+++ src/main/java/com/app/kidsdrawing/repository/SemesterClassRepository.java
@@ -17,19 +17,19 @@ public interface SemesterClassRepository extends JpaRepository <SemesterClass, L
     @Query("SELECT e FROM SemesterClass e JOIN FETCH e.semester  JOIN FETCH e.course ORDER BY e.id")
     List<SemesterClass> findAll();
 
-    @Query("SELECT  DISTINCT e FROM SemesterClass e JOIN FETCH e.semester  JOIN FETCH e.course c JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes JOIN FETCH c.user ORDER BY e.id")
+    @Query("SELECT  DISTINCT e FROM SemesterClass e JOIN FETCH e.semester  JOIN FETCH e.course c JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes  ORDER BY e.id")
     List<SemesterClass> findAll1();
 
-    @Query("SELECT DISTINCT e FROM SemesterClass e JOIN FETCH e.semester  JOIN FETCH e.course c JOIN FETCH e.userRegisterJoinSemesters urj JOIN FETCH urj.student st JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes JOIN FETCH c.user WHERE st.id = ?1 AND c.id =?2 ORDER BY e.id")
+    @Query("SELECT DISTINCT e FROM SemesterClass e JOIN FETCH e.semester  JOIN FETCH e.course c JOIN FETCH e.userRegisterJoinSemesters urj JOIN FETCH urj.student st JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes  WHERE st.id = ?1 AND c.id =?2 ORDER BY e.id")
     List<SemesterClass> findAllSemesterClassByStudentAndCourse(Long student_id, Long course_id);
 
     @Query("SELECT DISTINCT e FROM SemesterClass e JOIN FETCH e.semester LEFT JOIN FETCH e.userRegisterTeachSemesters urt LEFT JOIN FETCH urt.teacher te  JOIN FETCH e.course c JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes WHERE te.id = ?1 AND c.id =?2 ORDER BY e.id")
     List<SemesterClass> findAllSemesterClassByTeacherAndCourse(Long teacher_id, Long course_id);
 
-    @Query("SELECT DISTINCT e FROM SemesterClass e JOIN FETCH e.semester  JOIN FETCH e.course c JOIN FETCH e.userRegisterJoinSemesters urj JOIN FETCH urj.student st JOIN FETCH urj.payer pa JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes JOIN FETCH c.user WHERE pa.id = ?1 AND c.id =?2 AND urj.status = 'Completed' ORDER BY e.id")
+    @Query("SELECT DISTINCT e FROM SemesterClass e JOIN FETCH e.semester  JOIN FETCH e.course c JOIN FETCH e.userRegisterJoinSemesters urj JOIN FETCH urj.student st JOIN FETCH urj.payer pa JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes  WHERE pa.id = ?1 AND c.id =?2 AND urj.status = 'Completed' ORDER BY e.id")
     List<SemesterClass> findAllSemesterClassByParentAndCourse1(Long student_id, Long course_id);
 
-    @Query("SELECT DISTINCT e FROM SemesterClass e JOIN FETCH e.semester  JOIN FETCH e.course c JOIN FETCH e.userRegisterJoinSemesters urj JOIN FETCH urj.student st JOIN FETCH urj.payer pa JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes JOIN FETCH c.user WHERE pa.id = ?1 AND c.id =?2 AND urj.status = 'Waiting' ORDER BY e.id")
+    @Query("SELECT DISTINCT e FROM SemesterClass e JOIN FETCH e.semester  JOIN FETCH e.course c JOIN FETCH e.userRegisterJoinSemesters urj JOIN FETCH urj.student st JOIN FETCH urj.payer pa JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes  WHERE pa.id = ?1 AND c.id =?2 AND urj.status = 'Waiting' ORDER BY e.id")
     List<SemesterClass> findAllSemesterClassByParentAndCourse2(Long student_id, Long course_id);
 
     @Query(
@@ -63,9 +63,9 @@ public interface SemesterClassRepository extends JpaRepository <SemesterClass, L
     @Query("FROM SemesterClass e JOIN FETCH e.course co WHERE co.id = ?1 ORDER BY e.id")
     List<SemesterClass> findByCourseId1(Long id);
 
-    @Query("FROM SemesterClass e JOIN FETCH e.course co JOIN FETCH e.semester  WHERE co.id = ?1 ORDER BY e.id")
+    @Query("FROM SemesterClass e JOIN FETCH e.course c JOIN FETCH e.semester  JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes WHERE c.id = ?1 ORDER BY e.id")
     List<SemesterClass> findByCourseId2(Long id);
 
-    @Query("SELECT  DISTINCT e FROM SemesterClass e JOIN FETCH e.course c JOIN FETCH e.semester JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes JOIN FETCH c.user WHERE c.id = ?1 ORDER BY e.id")
+    @Query("SELECT  DISTINCT e FROM SemesterClass e JOIN FETCH e.course c JOIN FETCH e.semester JOIN FETCH e.schedules sch JOIN FETCH sch.lessonTime JOIN FETCH c.artAges JOIN FETCH c.artLevels JOIN FETCH c.artTypes WHERE c.id = ?1 ORDER BY e.id")
     List<SemesterClass> findByCourseId3(Long id);
 }
